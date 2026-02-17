8b34da29d5b6c7e3f6a2132cea6e31a9dabeee35
Pratik <pratiknpawar4912@gmail.com>
2026-02-17 09:44:11 +0530

    new changes

--- backend/routers/adminRoutes.js
+++ backend/routers/adminRoutes.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const { protect, adminOnly } = require("../middleware/authMiddleware");
-const { 
-    getAllClients, createClient, updateClient, deleteClient,
-    getAllSubmissions, getSubmissionsByClient, updateSubmission, deleteSubmission,
-    toggleClientAccess
+const {
+  getAllClients,
+  createClient,
+  updateClient,
+  deleteClient,
+  getAllSubmissions,
+  getSubmissionsByClient,
+  updateSubmission,
+  deleteSubmission,
+  toggleClientAccess,
+  resetClientPassword,
 } = require("../controllers/adminController");
 
 router.use(protect);
@@ -15,6 +22,7 @@ router.post("/clients", createClient);
 router.put("/clients/:id", updateClient);
 router.delete("/clients/:id", deleteClient);
 router.put("/clients/:id/toggle-access", toggleClientAccess);
+router.put("/clients/:id/reset-password", resetClientPassword);
 
 router.get("/submissions", getAllSubmissions);
 router.get("/submissions/client/:clientID", getSubmissionsByClient);
